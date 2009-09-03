72dedcafb338832c6225b030818c91038a3a6a8f
Aaron Quint <aaron@quirkey.com>
2009-09-03 00:15:54 -0400

    Basically an initial commit, Imported all my JS libs, started playing with jquery.couch

---
+++ _attachments/javascripts/app.js
@@ -0,0 +1,42 @@
+;(function($) {
+  
+  var dbname = window.location.pathname.split('/')[1];
+  var db     = $.couch.db(dbname); 
+  
+  Presentation = function(doc) {
+    var default_doc = {
+      name: "",
+      slides: [],
+      type: "presentation" 
+    };
+    this.database   = db;
+    this.attributes = $.extend(default_doc, doc);
+  }
+  
+  $.extend(Presentation.prototype, {
+    save: function(callback) {
+      this.database.saveDoc(this.attributes, {
+        success: function(resp) {
+          console.log('save success', resp);
+          $.extend(this.attributes, resp);
+          callback.apply(this, [resp]);
+        }
+      });
+    }
+  });
+  
+  var app = $.sammy(function() {
+    this.debug = true;
+    this.element_selector = '#container';
+    
+    this.get('#/', function() {
+      this.partial('templates/index.html.erb');
+    });
+    
+  });
+  
+  $(function() {
+    app.run('#/');
+  });
+
+})(jQuery);
---
+++ _attachments/javascripts/shjs/README.txt
@@ -0,0 +1,25 @@
+This is the README file for SHJS - Syntax Highlighting in JavaScript.
+
+This distribution contains the following files:
+
+sh_main.js
+  The main script.
+sh_main.min.js
+  The main script, compressed by YUI Compressor
+  (http://www.julienlecomte.net/yuicompressor/).
+sh_style.css
+  Default style sheet.
+css/*.css
+  Style sheets for different highlighting themes.
+css/*.min.css
+  Compressed versions of the above.
+lang/*.js
+  Scripts for different programming languages.
+lang/*.min.js
+  Compressed versions of the above.
+doc/
+  Documentation.
+
+See index.html for instructions.
+
+See gpl.txt for licensing terms.
