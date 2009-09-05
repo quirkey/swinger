7e99058d762b72d0f344f57a9aff5b5c26ed2a69
Aaron Quint <aaron@quirkey.com>
2009-09-05 18:37:37 -0400

    Sweet - slides actually save

--- _attachments/javascripts/app.js
+++ _attachments/javascripts/app.js
@@ -46,15 +46,23 @@
         }
       }));
     },
-    slide: function(num) {
+    slide: function(num, update) {
+      var s;
       if (this.attributes.slides[num]) {
-        return this.attributes.slides[num];
+        s = this.attributes.slides[num];
       } else {
-        return this.attributes.slides[num] = {
+        s = {
+          content_html: "",
           content: "",
           transition: "",
           position: num
         };
+      }
+      if (typeof update != 'undefined') {
+        // do update
+        $.extend(this.attributes.slides[num], s, update);
+      } else {
+        return s;
       }
     }
   });
@@ -63,11 +71,26 @@
     this.debug = true;
     this.element_selector = '#container';
     
+    var current_preso = {};
+    
     this.helpers({
+      withCurrentPreso: function(callback) {
+        var context = this;
+        if (current_preso._id == this.params.id) {
+          context.log('withCurrentPreso', 'using current', current_preso);
+          callback.apply(context, [current_preso]);
+        } else {
+          Preso.find(this.params.id, function(p) {
+            current_preso = p;
+            context.log('withCurrentPreso', 'found', current_preso);
+            callback.apply(context, [current_preso]);
+          });
+        }
+      },
       markdown: function(text) {
         return new Showdown.converter().makeHtml(text);
       }
-    })
+    });
     
     this.get('#/', function() {
       this.partial('templates/index.html.erb');
@@ -82,11 +105,22 @@
     });
     
     this.get('#/preso/:id/edit/:slide_id', function(e) {
-      e.preso = null;
-      Preso.find(this.params.id, function(loaded) { 
-        e.log('preso.find', loaded);
-        e.preso = loaded;
+      e.withCurrentPreso(function(preso) {
+        e.preso = preso;
         e.partial('templates/edit.html.erb', {slide: e.preso.slide(e.params.slide_id)});
+      });
+    });
+    
+    this.post('#/preso/:id/edit/:slide_id', function(e) {
+      e.withCurrentPreso(function(preso) {
+        preso.slide(e.params.slide_id, {
+          content: e.params['content'], 
+          content_html: e.markdown(e.params['content'])
+        });
+        preso.save(function(p) {
+          var next_id = parseInt(e.params.slide_id) + 1;
+          e.redirect(e.join('/', '#', 'preso', this.attributes._id, 'edit', next_id));
+        });
       });
     });
     
